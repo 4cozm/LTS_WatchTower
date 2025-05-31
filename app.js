20b8d260c6638c7bcd3a03dba938701cc0d9bec4
4cozm <49065386+4cozm@users.noreply.github.com>
2025-05-31 17:21:25 +0900

    refactor: 코드 스타일 통일을 위한 import 및 이벤트 핸들러 수정

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-import dotenv from 'dotenv';
-import http from 'http';
-import { Server } from 'socket.io';
-import { initTemplateJob } from './jobs/refreshTemplate.js';
-import { sendAlertTalk } from './services/aligoService.js';
-import { WebSocketWatcher } from './services/webSocketWatcher.js';
+import dotenv from "dotenv";
+import http from "http";
+import { Server } from "socket.io";
+import { initTemplateJob } from "./jobs/refreshTemplate.js";
+import { sendAlertTalk } from "./services/aligoService.js";
+import sendToNtfy from "./services/ntfyService.js";
+import { WebSocketWatcher } from "./services/webSocketWatcher.js";
 dotenv.config();
 
 const server = http.createServer();
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: "*",
   },
 });
 WebSocketWatcher(io); //웹 소켓 미연결 감시 시작
 
-io.on('connection', socket => {
+io.on("connection", (socket) => {
   console.log(`📡 클라이언트 연결: ${socket.id}`);
 
-  socket.on('sendAlertTalk', async data => {
-    console.log('📨 알림톡 요청 수신:', data);
+  socket.on("sendAlertTalk", async (data) => {
+    console.log("📨 알림톡 요청 수신:", data);
 
     try {
       const result = await sendAlertTalk(data);
-      socket.emit('sendSuccess', result);
+      socket.emit("sendSuccess", result);
     } catch (err) {
-      socket.emit('sendError', { error: err.message });
+      socket.emit("sendError", { error: err.message });
     }
   });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     console.log(`❌ 연결 종료: ${socket.id} 종료 시각:${Date.now()}`);
   });
 });
@@ -36,5 +37,6 @@ io.on('connection', socket => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, async () => {
   console.log(`🚀 WebSocket 서버 실행 중 (ws://localhost:${PORT})`);
+  sendToNtfy("server/start", { title: "Server Started", message: "Watch Tower Server is now online" });
   await initTemplateJob();
 });
