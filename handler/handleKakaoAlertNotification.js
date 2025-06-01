a300080d4cb1e24c0e1531d67aaae1af427cd0ab
4cozm <17328rm@gmail.com>
2025-06-02 00:27:26 +0900

    feat: add handler functions for Kakao alert notification, ping, pong, server status, and ntfy notification

---
+++ handler/handleKakaoAlertNotification.js
@@ -0,0 +1,1 @@
+export default function handleKakaoAlertNotification(payload, socket) {}
--- proto/protoHandler.js
+++ proto/protoHandler.js
@@ -1,3 +1,9 @@
+import handleKakaoAlertNotification from '../handler/handleKakaoAlertNotification.js';
+import handlePing from '../handler/handlePing.js';
+import handlePong from '../handler/handlePong.js';
+import handleServerStatus from '../handler/handleServerStatus.js';
+import handlentfyNotification from '../handler/handlentfyNotification.js';
+
 export const protoHandlers = {
   ping: handlePing,
   pong: handlePong,
